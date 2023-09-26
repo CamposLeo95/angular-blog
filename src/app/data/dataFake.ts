92fdd18b4c3c4c62ca0975086470c1cb86e8d252
CamposLeo95 <leocampos.995@gmail.com>
2023-09-26 20:25:14 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://images4.alphacoders.com/132/1329876.png",
+    "title": "Novo Filme Batman 2023",
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid odit dolorum nihil. Accusantium accusamus quod nesciunt consequatur atque fuga maxime fugit autem corporis. Adipisci."
+  },
+  {
+    "id": "2",
+    "photo": "https://static.ric.com.br/uploads/2016/07/marvel-logo-wallpaper.jpg",
+    "title": "Nova Serie One Piece",
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid odit dolorum nihil. Accusantium accusamus quod nesciunt consequatur atque fuga maxime fugit autem corporis. Adipisci."
+  },
+  {
+    "id": "3",
+    "photo": "https://static.ric.com.br/uploads/2016/07/marvel-logo-wallpaper.jpg",
+    "title": "Nova Serie One Piece",
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid odit dolorum nihil. Accusantium accusamus quod nesciunt consequatur atque fuga maxime fugit autem corporis. Adipisci."
+  },
+  {
+    "id": "4",
+    "photo": "https://static.ric.com.br/uploads/2016/07/marvel-logo-wallpaper.jpg",
+    "title": "Nova Serie One Piece",
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid odit dolorum nihil. Accusantium accusamus quod nesciunt consequatur atque fuga maxime fugit autem corporis. Adipisci."
+  }
+
+
+
+]
