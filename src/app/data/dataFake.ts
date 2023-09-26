dcbf68d31223906c4482fd24179c33150f76feb0
CamposLeo95 <leocampos.995@gmail.com>
2023-09-26 20:50:45 -0300

    refatorando home

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -9,19 +9,19 @@ export const dataFake = [
   {
     "id": "2",
     "photo": "https://static.ric.com.br/uploads/2016/07/marvel-logo-wallpaper.jpg",
-    "title": "Nova Serie One Piece",
+    "title": "Novas revistas da Marvel agitam o mundo nerd.",
     "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid odit dolorum nihil. Accusantium accusamus quod nesciunt consequatur atque fuga maxime fugit autem corporis. Adipisci."
   },
   {
     "id": "3",
-    "photo": "https://static.ric.com.br/uploads/2016/07/marvel-logo-wallpaper.jpg",
+    "photo": "https://mixdeseries.com.br/wp-content/uploads/2023/09/one-piece-quantas-temporadas.webp",
     "title": "Nova Serie One Piece",
     "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid odit dolorum nihil. Accusantium accusamus quod nesciunt consequatur atque fuga maxime fugit autem corporis. Adipisci."
   },
   {
     "id": "4",
-    "photo": "https://static.ric.com.br/uploads/2016/07/marvel-logo-wallpaper.jpg",
-    "title": "Nova Serie One Piece",
+    "photo": "https://www.gamespot.com/a/uploads/original/1597/15975876/4054278-age-of-mythology-retold-ann_10-25-22.jpg",
+    "title": "Microsoft anuncia novo age of mythology",
     "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid odit dolorum nihil. Accusantium accusamus quod nesciunt consequatur atque fuga maxime fugit autem corporis. Adipisci."
   }
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  dataFake = dataFake
 
+  mainId = dataFake[0].id
+  mainTitle = dataFake[0].title
+  mainPhoto = dataFake[0].photo
+  mainDescription = dataFake[0].description
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    this.removeArticleMain()
+  }
+
+  removeArticleMain(){
+     this.dataFake = dataFake.filter(article => article.id !== "1")
+  }
 }
